04a4682eb4d6c43dbf0d3a2b0906bea7176dd8ec
HozaifaJawhar <hozaifajawhar@gmail.com>
2025-07-24 01:24:01 +0300

    third commit

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -146,7 +146,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     onPressed: () {},
                     child: Text(
-                      'تسجيل ',
+                      'تسجيل الدخول',
                       style: GoogleFonts.almarai(
                         fontSize: UIHelpers.getResponsiveFontSize(
                           context,
