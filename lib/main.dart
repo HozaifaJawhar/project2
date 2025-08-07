8edd66aac9b31297333b529fea2a7ae35d722bbc
MissanAlifai <150163875+MissanAlifai@users.noreply.github.com>
2025-08-07 13:01:04 +0300

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:ammerha_volunteer/config/theme/app_theme.dart';
-import 'package:ammerha_volunteer/screens/events.dart';
-import 'package:ammerha_volunteer/screens/honor_board.dart';
-import 'package:ammerha_volunteer/screens/login.dart';
+
 import 'package:ammerha_volunteer/screens/mian_navigation_screen.dart';
-import 'package:ammerha_volunteer/screens/profile_screen.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
